3c1b3a4f3555e3c493253c68f74f8f451d1a26fb
reliable.methods <reliable.methods@8101b9f7-123e-0410-987b-a3817569d2f2>
2008-02-20 02:38:21 +0000

    Javadoc comments com.gcalsync.component.AboutComponent.java

--- src/com/gcalsync/component/AboutComponent.java
+++ src/com/gcalsync/component/AboutComponent.java
@@ -18,6 +18,17 @@ package com.gcalsync.component;
 import javax.microedition.lcdui.*;
 import javax.microedition.midlet.MIDlet;
 
+/**
+ * Public class responsible for drawing the About screen, fetching items to display 
+ * on the screen, and setting up the OK command button. User accesses this screen 
+ * by clicking the Menu button on the Login screen and selecting "About" from the menu. 
+ * This class uses the MVCComponent base class to retrieve, draw, and update screens.  
+ *   
+ * @author 
+ * @author 
+ * @version 
+ * @date 
+ */
 public class AboutComponent extends MVCComponent 
 {
 	static final Command CMD_OK = new Command("OK", Command.OK, 1);
@@ -55,7 +66,8 @@ public class AboutComponent extends MVCComponent
 	*/
     protected void createView() 
 	{
-		final StringItem lblVersion = new StringItem("Version", null);
+		//items that display on the page
+                final StringItem lblVersion = new StringItem("Version", null);
 		final StringItem lblBuildDate = new StringItem("Build date", null);
 		final StringItem lblProjectHomepage = new StringItem("Project homepage", null);
 		final StringItem lblDisclaimer = new StringItem("Disclaimer", null);
@@ -63,51 +75,61 @@ public class AboutComponent extends MVCComponent
 											Font.STYLE_BOLD,
 											Font.SIZE_SMALL);
 		String str;
-
+                //fetches the midlet name and assigns to str 
 		str = this.midlet.getAppProperty("MIDlet-Name");
+                //if string is not empty, str = About + Midlet 
 		if (str != null)
 			str = "About " + str;
+                //if string is empty, str = About
 		else
 			str = "About";
-
+                //display About form with value of str as title
 		this.form = new Form(str);
-
+                //add command listener for OK button
 		this.form.addCommand(CMD_OK);
 		this.form.setCommandListener(this);
 
 		try 
 		{
+                        //fetches midlet version and assigns to str
 			str = this.midlet.getAppProperty("MIDlet-Version");
-			if (str != null)
+			//if str is not empty
+                        if (str != null)
 			{
 				lblVersion.setFont(labelFont);
 				this.form.append(lblVersion);
-				this.form.append(str + " (ßeta)");
+				//display value of str + "Beta" on form through append
+                                this.form.append(str + " (ßeta)");
 				this.form.append(new Spacer(getDisplayable().getWidth(), 5));
 			}
-
+                        //fetches midlet build date and assigns to str
 			str = this.midlet.getAppProperty("Build-Date");
-			if (str != null)
+			//if str is not empty
+                        if (str != null)
 			{
 				lblBuildDate.setFont(labelFont);
 				this.form.append(lblBuildDate);
-				this.form.append(str);
+				//display value of str on form through append
+                                this.form.append(str);
 				this.form.append(new Spacer(getDisplayable().getWidth(), 5));
 			}
-
+                        //fetches midlet project URL and assigns to str
 			str = this.midlet.getAppProperty("MIDlet-Info-URL");
-			if (str != null)
+			//if str is not empty
+                        if (str != null)
 			{
 				lblProjectHomepage.setFont(labelFont);
 				this.form.append(lblProjectHomepage);
-				this.form.append(str);
+				//display value of str on form through append
+                                this.form.append(str);
 				this.form.append(new Spacer(getDisplayable().getWidth(), 5));
 			}
 		}
 		catch (Exception e) {}
 
 		lblDisclaimer.setFont(labelFont);
-		this.form.append(lblDisclaimer);
+		//display Disclaimer on form through append
+                this.form.append(lblDisclaimer);
 		this.form.append("THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.");
     }
 
